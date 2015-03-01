ff39c0177cac0572d0926a6dbf62ff80f611f98d
Calem Bendell <calem.j.bendell@gmail.com>
2015-02-28 22:59:13 -0500

    updated deftnetworking, work on physics

---
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public enum PlayerCharacter {  Syphen, Blitz };
+
+public class InGameStats : MonoBehaviour
+{
+  public int[] playerCurrentHealth;
+  public int[] playerTotalHealth;
+  public int numbersOfDepots;
+  public int[] depotCapacity;
+  public int[] depotCurrentStock;
+  public int[] depotResourceValue;
+  public GameObject[] depots;
+  public int killerAttackDamage;
+  public int feederAttackDamage;
+  public GameObject[] depotUI_objects;
+  public GameObject eventSystemObject;
+  public GameObject gameOverFirstSelected;
+  public GameObject gameOverWindow;
+  public GameObject winText;
+  public GameObject loseText;
+
+  private int depotsFull;
+  private EventSystem eventSystem;
+
+  void Start()
+  {
+    gameOverWindow.SetActive(false);
+    winText.SetActive(false);
+    loseText.SetActive(false);
+    eventSystem = eventSystemObject.GetComponent<EventSystem>();
+    depotsFull = 0;
+    //Disable all depots except the first one
+    foreach (GameObject d in depots)
+    {
+      d.SetActive(false);
+    }
+    foreach (GameObject d in depotUI_objects)
+    {
+      d.SetActive(false);
+    }
+    depots[0].SetActive(true);
+    depotUI_objects[0].SetActive(true);
+  }
+
+  public void decreaseHealth(string targetPlayerName, string attackerName)
+  {
+    int damage;
+    if (attackerName.Contains("Feeder"))
+    {
+      damage = feederAttackDamage;
+    }
+    else
+    {
+      damage = killerAttackDamage;
+    }
+    if (targetPlayerName.Contains("Syphen"))
+    {
+      playerCurrentHealth[0] -= damage;
+    }
+    else
+    {
+      playerCurrentHealth[1] -= damage;
+    }
+    if (playerCurrentHealth[0] <= 0 || playerCurrentHealth[1] <= 0)
+    {
+      gameOver();
+    }
+  }
+
+  //Increases resource count of a certain depot
+  public void increaseResourceCount(int depotNumber)
+  {
+    depotCurrentStock[depotNumber] += depotResourceValue[depotNumber];
+  }
+  public void depotFull()
+  {
+    depotsFull++;
+    if (depotsFull == numbersOfDepots)
+    {
+      lastDepotFull();
+    }
+    else
+    {
+      //Activate next depot
+      Debug.Log("A Depot is FULL");
+      depots[depotsFull].SetActive(true);
+      depotUI_objects[depotsFull].SetActive(true);
+    }
+  }
+  public void lastDepotFull()
+  {
+    Debug.Log("YOU WIN.");
+    gameOverWindow.SetActive(true);
+    winText.SetActive(true);
+    eventSystem.SetSelectedGameObject(gameOverFirstSelected);
+  }
+  public void gameOver()
+  {
+    Debug.Log("YOU DIED.");
+    gameOverWindow.SetActive(true);
+    loseText.SetActive(true);
+    eventSystem.SetSelectedGameObject(gameOverFirstSelected);
+  }
+
+
+}
---
+++ Assets/Scripts/Physics/GravityNodeBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GravityNodeType { Push, Pull, Lift }
+
+public class GravityNodeBehaviour : MonoBehaviour
+{
+
+  public float magnitudeBirth;
+  public float magnitudeDeath;
+  public float magnitudeRatio;
+  public float radiusBirth;
+  public float radiusDeath;
+  public float radiusRatio;
+  public float duration;
+  public GravityNodeType type;
+
+  public bool timed;
+
+  float currentRadius;
+  float currentMagnitude;
+
+
+  void Start()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    if (timed)
+    {
+      currentRadius = Mathf.Lerp(magnitudeBirth, magnitudeDeath, radiusRatio * Time.deltaTime);
+      currentMagnitude = Mathf.Lerp(radiusBirth, radiusDeath, radiusRatio * Time.deltaTime);
+      duration -= Time.deltaTime;
+    }
+  }
+
+  void OnTriggerStay(Collider other)
+  {
+    if (other.rigidbody.useGravity)
+    {
+      if (other.attachedRigidbody)
+      {
+        PhysicsStatus status = other.gameObject.GetComponent<PhysicsStatus>();
+        Vector3 direction = Vector3.Normalize(other.transform.position - this.transform.position);
+        switch (type)
+        {
+          case GravityNodeType.Lift:
+            if (status == null || status.liftable)
+            {
+              other.rigidbody.AddForce(direction * (currentMagnitude / Vector3.Magnitude(other.transform.position - this.transform.position)), ForceMode.Impulse);
+            }
+            break;
+          case GravityNodeType.Pull:
+            if (status == null || status.liftable)
+            {
+              other.rigidbody.AddForce(direction * (currentMagnitude / Vector3.Magnitude(other.transform.position - this.transform.position)), ForceMode.Impulse);
+            }
+            break;
+          case GravityNodeType.Push:
+            if (status == null || status.liftable)
+            {
+              other.rigidbody.AddForce(direction * (currentMagnitude / Vector3.Magnitude(other.transform.position - this.transform.position)), ForceMode.Impulse);
+            }
+            break;
+        }
+      }
+    }
+  }
+
+}
