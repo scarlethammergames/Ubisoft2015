c92a398febf3148fa619685d6dfac4b153838ca0
Marc Jarvis <jarvis.marc@gmail.com>
2015-03-02 14:09:24 -0500

    Added beam attack animation. Modified playerCtrl.
    PlayerController modified so that walking and idle animations are two
    separate things.
    
    	modified:   Assets/DeftNetworking/networked-third-person-player-controller-copy/Scripts/DeftPlayerCamera.cs
    	modified:   Assets/DeftNetworking/networked-third-person-player-controller-copy/Scripts/DeftPlayerController.cs
    	modified:   Assets/Models/characters/Blitz/BlitzController.controller
    	new file:   Assets/Models/characters/Blitz/anim_Blitz_beamAttack.fbx
    	new file:   Assets/Models/characters/Blitz/anim_Blitz_beamAttack.fbx.meta
    	modified:   Assets/Prefabs/Characters/Blitz.prefab
    	modified:   Assets/Prefabs/Characters/Syphen.prefab
    	modified:   Assets/Scenes/CharacterAnimationTests.unity
    	modified:   Assets/Scripts/Character/CharacterAnimator.cs

--- Assets/Scripts/Character/CharacterAnimator.cs
+++ Assets/Scripts/Character/CharacterAnimator.cs
@@ -6,30 +6,43 @@ public class CharacterAnimator : MonoBehaviour {
 	public float _sprintThreshold = 10f;
 
 	Animator _animator;
-  //DeftPlayerController controller;
+  	DeftPlayerController _controller;
 	Rigidbody _rb;
+	string[] _animationBoolParameters = {"isIdle", "isRunning", "isAttacking_Projectile"};
 
 	// Use this for initialization
 	void Start () {
 		_animator = this.GetComponent<Animator>();
 		_animator.SetBool("isIdle", true);
-    	 _rb = this.GetComponent<Rigidbody>();
+		_animator.SetBool("isRunning", false);
+		_controller = this.GetComponent<DeftPlayerController>();
+    	_rb = this.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(_animator)
 		{
-			float speed = _rb.GetPointVelocity(Vector3.zero).magnitude;
-			if(speed > _runThreshold)
+			//float speed = _rb.GetPointVelocity(Vector3.zero).magnitude;
+			if(_controller.state == PlayerState.walking)
 			{
-				_animator.SetBool("isIdle", false);
-				_animator.SetBool("isRunning", true);
+				transition(1);
+			}
+			else if(_controller.state == PlayerState.aiming){
+				transition(2);
 			}
 			else
 			{
-				_animator.SetBool("isIdle", true);
-				_animator.SetBool("isRunning", false);
+				transition(0);
+			}
+		}
+	}
+
+	void transition(int parameterIndex){
+		_animator.SetBool(_animationBoolParameters[parameterIndex], true);
+		for(int i = 0; i < _animationBoolParameters.Length; ++i){
+			if(i != parameterIndex){
+				_animator.SetBool(_animationBoolParameters[i], false);
 			}
 		}
 	}
