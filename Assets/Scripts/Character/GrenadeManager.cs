ebaa42ef53cba39ed440647f067829b0cd3f46a2
Jordan Quan <quan.jordan@gmail.com>
2015-03-02 17:28:24 -0500

    Grenades for days!
    Still need to integrate this with the proper scripts.

---
+++ Assets/Scripts/Character/GrenadeManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//add to empty game object and attach in front (offset) of Blitz
+
+public class GrenadeManager : MonoBehaviour {
+	
+	public int currentAmmo;
+	public int maxAmmo;
+	public float recharge;
+	public float timer = 0;
+	public GameObject prefab; //insert grenade prefab here
+	public Vector3 offset; //unused
+	public float magnitude; //throwing velocity
+	
+	void Start () {
+		
+	}
+	//need to change input to controller
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (currentAmmo > 0)
+			{
+				throwGrenade();
+				currentAmmo --;
+			}
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		if (currentAmmo < maxAmmo) 
+		{
+			if (timer > recharge){
+				currentAmmo ++;
+				timer = 0;
+			}
+			timer += Time.deltaTime;
+			
+		}
+	}
+	
+	void throwGrenade()
+	{
+		GameObject clone;
+		clone = Instantiate (prefab, transform.position + offset, transform.rotation) as GameObject;
+		Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+		forward = forward.normalized;
+		clone.rigidbody.velocity = (new Vector3(forward.x * magnitude, 0, forward.z * magnitude));
+	}
+}
---
+++ Assets/Scripts/Physics/explosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion : MonoBehaviour {
+	public float radius;
+	public float power;
+	public float lift;
+	public float delay;
+	
+	void Start(){
+		StartCoroutine ("MyMethod");
+	}
+
+	IEnumerator MyMethod() {
+		yield return new WaitForSeconds(delay);
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		
+		foreach (Collider hit in colliders) {
+			// can add cases for non physics objects later
+			if (hit && hit.rigidbody){
+				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, lift, ForceMode.Impulse); //if we hit any rigidbodies then add force based off our power, the position of the explosion object
+			}
+		}
+		Destroy (gameObject);
+	}
+}
