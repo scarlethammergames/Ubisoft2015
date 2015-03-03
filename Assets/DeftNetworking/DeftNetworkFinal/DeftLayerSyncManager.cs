8a173af4c29a5ae25dd817b42fa2689e151a6e46
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-03 00:21:48 -0500

    added deft sync layer manager

---
+++ Assets/DeftNetworking/DeftNetworkFinal/DeftLayerSyncManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DeftLayerSyncManager : MonoBehaviour {
+
+    public Dictionary<int, GameObject> objectsInLayer;
+
+    [RPC]
+    public void SetObjectsInLayer()
+    {
+
+    }
+
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+	
+	}
+}
