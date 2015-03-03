6cfa455afea54ae1fb91b14b12555ea7c77c4eb6
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-02 21:46:18 -0500

    further nuking and moving for submodules

---
+++ Assets/DeftNetworking/Assets/Scripts/Demo/TestingThrusters.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingThrusters : MonoBehaviour {
+
+  [RPC]
+  public void Activate()
+  {
+    Debug.Log("Activating thrusters.");
+  }
+
+}
