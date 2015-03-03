f3c470600106f17674537972e0023031660164bd
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-03 01:21:54 -0500

    addded queue in sync manager and got rpc working

--- Assets/DeftNetworking/DeftNetworkFinal/DeftClientServerHandler.cs
+++ Assets/DeftNetworking/DeftNetworkFinal/DeftClientServerHandler.cs
@@ -1,70 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DeftClientServerHandler : MonoBehaviour {
+public class DeftClientServerHandler : MonoBehaviour
+{
 
-  public enum DeftNetworkRole { SEARCHER, CLIENT, WILLHOST, UNASSIGNED, HOST };
-  public DeftNetworkRole currentRole = DeftNetworkRole.UNASSIGNED;
+    public enum DeftNetworkRole { SEARCHER, CLIENT, WILLHOST, UNASSIGNED, HOST };
+    public DeftNetworkRole currentRole = DeftNetworkRole.UNASSIGNED;
 
-  public bool LAN = true;
-  public int numberConnections = 4;
-  public int port = 2500;
+    public bool LAN = true;
+    public int numberConnections = 4;
+    public int port = 2500;
 
-  public string gameName = "4f5re3";
-  public string roomName = "15e6fs";
-  public string serverAddress = "127.0.0.1";
+    public string gameName = "4f5re3";
+    public string roomName = "15e6fs";
+    public string serverAddress = "127.0.0.1";
 
-  HostData[] hostdata;
+    HostData[] hostdata;
 
-  void Start()
-  {
-    Application.runInBackground = true;
-  }
-
-  void OnGUI()
-  {
-    if (!Network.isClient && !Network.isServer)
+    void Start()
     {
-      if (GUI.Button(new Rect(10, 10, 150, 50), "Join Server"))
-      {
-        this.currentRole = DeftNetworkRole.SEARCHER;
-      }
-      if (GUI.Button(new Rect(10, 70, 150, 50), "Host Server"))
-      {
-        this.currentRole = DeftNetworkRole.WILLHOST;
-      }
+        Application.runInBackground = true;
     }
-  }
 
-  void Update()
-  {
-    switch (this.currentRole)
+    void OnGUI()
     {
-      case DeftNetworkRole.WILLHOST:
-        Network.InitializeServer(this.numberConnections, this.port, !this.LAN);
-        MasterServer.RegisterHost(this.gameName, this.roomName);
-        this.currentRole = DeftNetworkRole.HOST;
-        break;
-      case DeftNetworkRole.SEARCHER:
-        MasterServer.RequestHostList(this.gameName);
-        HostData[] hostdata = MasterServer.PollHostList();
-        if (hostdata.Length > 0)
+        if (!Network.isClient && !Network.isServer)
         {
-          Network.Connect(hostdata[0]);
+            if (GUI.Button(new Rect(10, 10, 150, 50), "Join Server"))
+            {
+                this.currentRole = DeftNetworkRole.SEARCHER;
+            }
+            if (GUI.Button(new Rect(10, 70, 150, 50), "Host Server"))
+            {
+                this.currentRole = DeftNetworkRole.WILLHOST;
+            }
+            if (this.currentRole == DeftNetworkRole.SEARCHER)
+            {
+                for (int i = 0; i < hostdata.Length; i++)
+                {
+                    if (GUI.Button(new Rect(160, 10 + 50 * i, 150, 50), hostdata[i].gameName))
+                    {
+                        Network.Connect(this.hostdata[i]);
+                    }
+                }
+            }
         }
-        break;
     }
-  }
+
+    void Update()
+    {
+        switch (this.currentRole)
+        {
+            case DeftNetworkRole.WILLHOST:
+                Network.InitializeServer(this.numberConnections, this.port, !this.LAN);
+                MasterServer.RegisterHost(this.gameName, this.roomName);
+                this.currentRole = DeftNetworkRole.HOST;
+                break;
+            case DeftNetworkRole.SEARCHER:
+                MasterServer.RequestHostList(this.gameName);
+                hostdata = MasterServer.PollHostList();
+                if (hostdata.Length > 0)
+                {
+                    //Debug.Log("Connecting to " + hostdata[0].ToString());
+                    //Network.Connect(hostdata[0]);
+                }
+                break;
+        }
+    }
 
 
-  void OnFailedToConnect(NetworkConnectionError error)
-  {
-    Debug.Log("Could not connect to server: " + error);
-  }
+    void OnFailedToConnect(NetworkConnectionError error)
+    {
+        Debug.Log("Could not connect to server: " + error);
+    }
 
-  void OnConnectedToServer()
-  {
-    Debug.Log("Successfully connected to server.");
-    this.currentRole = DeftNetworkRole.CLIENT;
-  }
+    void OnConnectedToServer()
+    {
+        Debug.Log("Successfully connected to server.");
+        this.currentRole = DeftNetworkRole.CLIENT;
+    }
 }
--- Assets/DeftNetworking/DeftNetworkFinal/RPCScratch.cs
+++ Assets/DeftNetworking/DeftNetworkFinal/RPCScratch.cs
@@ -1,31 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
+using System;
 
-public class RPCScratch : MonoBehaviour {
+public class RPCScratch : MonoBehaviour
+{
 
-  public int i;
-
-  [RPC]
-  void setI(int newI)
-  {
-    this.i = newI;
-    Debug.Log("Set i to " + this.i);
-  }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-    if (i % 25 == 0)
+    [StructLayout(LayoutKind.Sequential)]
+    struct RPCBodyState
     {
-      if(Network.isServer)
-      {
-        this.networkView.RPC("setI", RPCMode.Others, this.i);
-      }
+        public double timestamp;
+        public Vector3 pos;
+        public Vector3 velocity;
+        public Quaternion rot;
+        public Vector3 angularVelocity;
     }
-    i++;
-	}
+
+    byte[] MarshalBodyState(RPCBodyState state)
+    {
+        int size = Marshal.SizeOf(state);
+        byte[] arr = new byte[size];
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        Marshal.StructureToPtr(state, ptr, true);
+        Marshal.Copy(ptr, arr, 0, size);
+        Marshal.FreeHGlobal(ptr);
+        return arr;
+    }
+
+    RPCBodyState UnMarshalBodyState(byte[] arr)
+    {
+        RPCBodyState state = new RPCBodyState();
+        int size = Marshal.SizeOf(state);
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        Marshal.Copy(arr, 0, ptr, size);
+        state = (RPCBodyState)Marshal.PtrToStructure(ptr, state.GetType());
+        Marshal.FreeHGlobal(ptr);
+        return state;
+    }
+
+    public int i;
+
+    [RPC]
+    void setI(int newI)
+    {
+        this.i = newI;
+        Debug.Log("Set i to " + this.i);
+    }
+
+    [RPC]
+    void ReceiveBodyState(byte[] bytes)
+    {
+        RPCBodyState state = UnMarshalBodyState(bytes);
+        Debug.Log("Received state with stamp " + state.timestamp + " and angular velocity " + state.angularVelocity);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        {
+            if (Network.isServer)
+            {
+                RPCBodyState state = new RPCBodyState();
+                state.timestamp = i;
+                state.angularVelocity = new Vector3(0f, 1.2f, 4f);
+                this.networkView.RPC("ReceiveBodyState", RPCMode.Others, MarshalBodyState(state));
+            }
+        }
+        i++;
+    }
 }
