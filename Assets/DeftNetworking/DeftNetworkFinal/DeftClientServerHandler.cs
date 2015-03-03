d76e5c47a5706958c8ef56edb75e45ba861ffb5e
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-03 00:07:23 -0500

    networking now working with clientserverhandler

--- Assets/DeftNetworking/DeftNetworkFinal/DeftClientServerHandler.cs
+++ Assets/DeftNetworking/DeftNetworkFinal/DeftClientServerHandler.cs
@@ -3,13 +3,68 @@ using System.Collections;
 
 public class DeftClientServerHandler : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  public enum DeftNetworkRole { SEARCHER, CLIENT, WILLHOST, UNASSIGNED, HOST };
+  public DeftNetworkRole currentRole = DeftNetworkRole.UNASSIGNED;
+
+  public bool LAN = true;
+  public int numberConnections = 4;
+  public int port = 2500;
+
+  public string gameName = "4f5re3";
+  public string roomName = "15e6fs";
+  public string serverAddress = "127.0.0.1";
+
+  HostData[] hostdata;
+
+  void Start()
+  {
+    Application.runInBackground = true;
+  }
+
+  void OnGUI()
+  {
+    if (!Network.isClient && !Network.isServer)
+    {
+      if (GUI.Button(new Rect(10, 10, 150, 50), "Join Server"))
+      {
+        this.currentRole = DeftNetworkRole.SEARCHER;
+      }
+      if (GUI.Button(new Rect(10, 70, 150, 50), "Host Server"))
+      {
+        this.currentRole = DeftNetworkRole.WILLHOST;
+      }
+    }
+  }
+
+  void Update()
+  {
+    switch (this.currentRole)
+    {
+      case DeftNetworkRole.WILLHOST:
+        Network.InitializeServer(this.numberConnections, this.port, !this.LAN);
+        MasterServer.RegisterHost(this.gameName, this.roomName);
+        this.currentRole = DeftNetworkRole.HOST;
+        break;
+      case DeftNetworkRole.SEARCHER:
+        MasterServer.RequestHostList(this.gameName);
+        HostData[] hostdata = MasterServer.PollHostList();
+        if (hostdata.Length > 0)
+        {
+          Network.Connect(hostdata[0]);
+        }
+        break;
+    }
+  }
+
+
+  void OnFailedToConnect(NetworkConnectionError error)
+  {
+    Debug.Log("Could not connect to server: " + error);
+  }
+
+  void OnConnectedToServer()
+  {
+    Debug.Log("Successfully connected to server.");
+    this.currentRole = DeftNetworkRole.CLIENT;
+  }
 }
---
+++ Assets/DeftNetworking/DeftNetworkFinal/RPCScratch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPCScratch : MonoBehaviour {
+
+  public int i;
+
+  [RPC]
+  void setI(int newI)
+  {
+    this.i = newI;
+    Debug.Log("Set i to " + this.i);
+  }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    if (i % 25 == 0)
+    {
+      if(Network.isServer)
+      {
+        this.networkView.RPC("setI", RPCMode.Others, this.i);
+      }
+    }
+    i++;
+	}
+}
