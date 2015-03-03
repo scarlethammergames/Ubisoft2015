845ea51f8b5a27e46d61a6d872ef2b2f089eaf92
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-02 22:51:10 -0500

    thrust in forward direction

--- Assets/DeftNetworking/Assets/Scripts/Demo/TestingThrusters.cs
+++ Assets/DeftNetworking/Assets/Scripts/Demo/TestingThrusters.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class TestingThrusters : MonoBehaviour {
 
+  public float thrusterPower;
+
   [RPC]
   public void Activate()
   {
     Debug.Log("Activating thrusters.");
+
+    this.gameObject.rigidbody.AddForce(this.gameObject.transform.forward * thrusterPower);
+
   }
 
 }
