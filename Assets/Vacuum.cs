54d077fc143cbbf192b597ab871296014896f9e8
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-06 10:34:18 -0500

    vacuum broken, need to destroy the laser

--- Assets/Vacuum.cs
+++ Assets/Vacuum.cs
@@ -2,44 +2,71 @@
 using System.Collections;
 using GamepadInput;
 
-public class Vacuum : MonoBehaviour {
+public class Vacuum : MonoBehaviour
+{
 
-	public float magnitude;
-	private float trigger;
-	private DeftPlayerController controller;
-	public bool affectEverything;
-	private ParticleEmitter effect;
+  public float magnitude;
+  private float trigger;
+  private DeftPlayerController controller;
+  public bool affectEverything;
+  private ParticleEmitter effect;
+  public bool active;
 
-	void Start () {
-		controller = GameObject.FindGameObjectWithTag("Player").GetComponent<DeftPlayerController>();
-		effect = GetComponentInChildren<ParticleEmitter> ();
-		effect.emit = false;
-	}
+  void Start()
+  {
+    controller = GameObject.FindGameObjectWithTag("Player").GetComponent<DeftPlayerController>();
+    effect = GetComponentInChildren<ParticleEmitter>();
+    effect.emit = false;
+  }
 
 
-	void Update () {
-		trigger = controller.gamepadState.RightTrigger;
-		if (trigger > 0) effect.emit = true;
-		else effect.emit = false;
+  [RPC]
+  void Activate()
+  {
+    this.active = true;
+  }
 
-		//Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
-		//this.transform.LookAt (Camera.main.transform.position);
-		//transform.rotation = Quaternion.LookRotation (-Camera.main.transform.forward, Camera.main.transform.right);
-	}
+  [RPC]
+  void DeActivate()
+  {
+    this.active = false;
+  }
 
-	void OnTriggerStay(Collider other)
-	{
-		trigger = controller.gamepadState.RightTrigger;
-		if (trigger > 0) {
-			if (other.rigidbody.useGravity && other.tag != "Player") {
-				PhysicsStatus status = other.gameObject.GetComponent<PhysicsStatus> ();
-				Vector3 direction = Vector3.Normalize (other.transform.position - this.transform.parent.position);
-				if (status == null || status.pullable || affectEverything) {
-					other.rigidbody.AddForce (direction * (-1 * magnitude), ForceMode.Impulse);
-				}
-			}
-		} 
+  void Update()
+  {
+    if (this.networkView.isMine)
+    {
+      if (controller.gamepadState.RightTrigger > 0.20f && !this.active)
+      {
+        this.networkView.RPC("Activate", RPCMode.Others);
+        this.Activate();
+      }
+      else if (controller.gamepadState.RightTrigger > 0.20f && this.active)
+      {
+      }
+      else
+      {
+        this.networkView.RPC("DeActivate", RPCMode.Others);
+        this.DeActivate();
+      }
+    }
+    //Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+    //this.transform.LookAt (Camera.main.transform.position);
+    //transform.rotation = Quaternion.LookRotation (-Camera.main.transform.forward, Camera.main.transform.right);
+  }
 
-	}
+  void OnTriggerStay(Collider other)
+  {
+    if (active)
+      if (other.rigidbody.useGravity && other.tag != "Player")
+      {
+        PhysicsStatus status = other.gameObject.GetComponent<PhysicsStatus>();
+        Vector3 direction = Vector3.Normalize(other.transform.position - this.transform.parent.position);
+        if (status == null || status.pullable || affectEverything)
+        {
+          other.rigidbody.AddForce(direction * (-1 * magnitude), ForceMode.Impulse);
+        }
+      }
+  }
 
 }
