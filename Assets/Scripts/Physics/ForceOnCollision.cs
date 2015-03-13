d49c273487ab5d8e2afdc537fa0874548ece422f
Marc Jarvis <jarvis.marc@gmail.com>
2015-03-13 19:02:21 -0400

    Tweaked Syphen cooldown times.
    	modified:   Assets/Prefabs/Characters/Syphen.prefab
    	modified:   Assets/Prefabs/Powers/Pull_Controllable.prefab
    	modified:   Assets/Prefabs/Powers/Pull_Trail.prefab
    	new file:   Assets/Prefabs/Powers/Pull_Trail_NoVFX.prefab
    	new file:   Assets/Prefabs/Powers/Pull_Trail_NoVFX.prefab.meta
    	modified:   Assets/Scenes/CharacterAttackTests.unity
    	modified:   Assets/Scripts/Physics/ForceOnCollision.cs

--- Assets/Scripts/Physics/ForceOnCollision.cs
+++ Assets/Scripts/Physics/ForceOnCollision.cs
@@ -2,20 +2,13 @@
 using System.Collections;
 
 public class ForceOnCollision: MonoBehaviour {
-	public float _magnitudeBirth = 100;
-	public float _magnitudeDeath = 0;
-	public float _magRatio = 1;
-	public float _radiusBirth = 100;
-	public float _radiusDeath = 0;
-	public float _radiusRatio = 1;
-	public int _maxDuration = 100;
+	public float _magnitude = 100;
+	public float _radius = 100;
+	public int _duration = 100;
 
 	public enum ForceType {Push, Pull, Lift};
 	public ForceType _forceType = ForceType.Push;
 	
-	private int _duration;
-	private float _currentRadius;
-	private float _currentMagnitude;
 	private int _maxMagnitude = 1000000;
 
 	public float _delayTime = 5.0f;
@@ -25,17 +18,16 @@ public class ForceOnCollision: MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		_currentMagnitude = _magnitudeBirth;
-		_currentRadius = _radiusBirth;
-		_duration = _maxDuration;
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if( _duration > 0 ){
 			_duration--;
-			_currentRadius = Mathf.Lerp( _magnitudeBirth, _magnitudeDeath, _radiusRatio * Time.deltaTime );
-			_currentMagnitude = Mathf.Lerp( _radiusBirth, _radiusDeath, _radiusRatio * Time.deltaTime );
+		}
+		else{
+			Destroy(this.gameObject);
 		}
 	}
 
@@ -46,7 +38,7 @@ public class ForceOnCollision: MonoBehaviour {
 		//Debugging
 		if(debug){
 			Gizmos.color = Color.yellow;
-			Gizmos.DrawSphere(transform.position, _currentRadius);
+			Gizmos.DrawSphere(transform.position, _radius);
 		}
 	}
 
@@ -63,7 +55,7 @@ public class ForceOnCollision: MonoBehaviour {
 						}
 						else if(other.attachedRigidbody){
 							Vector3 direction = Vector3.Normalize( other.transform.position - this.transform.position );
-							other.rigidbody.AddForce( direction * Mathf.Clamp(_currentMagnitude/Vector3.Magnitude(  other.transform.position - this.transform.position ), 0, _maxMagnitude) , ForceMode.Impulse);
+							other.rigidbody.AddForce( direction * Mathf.Clamp(_magnitude/Vector3.Magnitude(  other.transform.position - this.transform.position ), 0, _maxMagnitude) , ForceMode.Impulse);
 							other.GetComponent<PhysicsStatus>().pullable = true;
 						}
 					}				
@@ -72,14 +64,14 @@ public class ForceOnCollision: MonoBehaviour {
 					if( ps.pullable ){
 						if(other.attachedRigidbody){
 							Vector3 direction = Vector3.Normalize( this.transform.position - other.transform.position );
-							other.rigidbody.AddForce( direction * Mathf.Clamp(_currentMagnitude/Vector3.Magnitude(this.transform.position - other.transform.position), 0, _maxMagnitude) , ForceMode.Impulse);
+							other.rigidbody.AddForce( direction * Mathf.Clamp(_magnitude/Vector3.Magnitude(this.transform.position - other.transform.position), 0, _maxMagnitude) , ForceMode.Impulse);
 						}
 					}		
 					break;
 				case ForceType.Lift:
 					if( ps.liftable ){
 						if(other.attachedRigidbody){
-							other.rigidbody.AddForce( Vector3.up * Mathf.Clamp(_currentMagnitude/Vector3.Magnitude(this.transform.position - other.transform.position), 0, _maxMagnitude) , ForceMode.Impulse);
+							other.rigidbody.AddForce( Vector3.up * Mathf.Clamp(_magnitude/Vector3.Magnitude(this.transform.position - other.transform.position), 0, _maxMagnitude) , ForceMode.Impulse);
 						}
 					}
 					break;
