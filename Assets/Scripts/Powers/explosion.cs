9b1ec157de9cc23a9599572f0aedca34680453b2
Jordan Quan <quan.jordan@gmail.com>
2015-03-13 18:42:41 -0400

    grenade explosions now shatters shatterables

--- Assets/Scripts/Powers/explosion.cs
+++ Assets/Scripts/Powers/explosion.cs
@@ -14,14 +14,25 @@ public class explosion : MonoBehaviour {
 
 	IEnumerator MyMethod() {
 		yield return new WaitForSeconds(delay);
+
 		Instantiate(effect, transform.position, transform.rotation);
 		Vector3 explosionPos = transform.position;
+
 		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
-		
 		foreach (Collider hit in colliders) {
 			// can add cases for non physics objects later
+			PhysicsStatus ps = (PhysicsStatus) hit.GetComponent<PhysicsStatus>(); // grab physics status of object
+			if( ps && ps.pushable ){ 
+				Shatterable shatterable = hit.gameObject.GetComponent<Shatterable>();
+				if(shatterable){
+					shatterable.switchToFractured(); // shatter shatterables
+				} else if(hit.attachedRigidbody){
+					hit.GetComponent<PhysicsStatus>().pullable = true; // switch objects to pullable
+				}
+			}
 			if (hit && hit.rigidbody){
-				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, lift, ForceMode.Impulse); //if we hit any rigidbodies then add force based off our power, the position of the explosion object
+				//and then still apply explosion force to any rigidbodies
+				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, lift, ForceMode.Impulse); 
 			}
 		}
 		Destroy (gameObject);
